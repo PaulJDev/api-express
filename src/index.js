9b6b0a3020a3776d10c9347e39ab4a33bdcb0198
Paul <pjavaloy@emeal.nttdata.com>
2022-07-21 10:15:23 +0200

    Add v1 route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 require('dotenv').config();
+const v1Router = require('./v1/routes');
 
-const PORT = process.env.PORT || 3000;
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
     res.send('Hello World');
 })
 
+app.use('/api/v1', v1Router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ src/v1/routes/index.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.route('/').get((req, res) => {
+    res.send('Ey! ' + req.baseUrl);
+})
+
+module.exports = router
