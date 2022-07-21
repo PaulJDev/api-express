e7d109d2f79c6939e25f715c0bcdc2fb399a98f0
Paul <pjavaloy@emeal.nttdata.com>
2022-07-21 09:43:00 +0200

    Running

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
